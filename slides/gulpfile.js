e3aa06bfb060445db098202eae53026a9cfac858
Tamara Temple <tamouse@gmail.com>
2016-10-30 22:31:50 -0500

    working on staging push

--- slides/gulpfile.js
+++ slides/gulpfile.js
@@ -5,16 +5,28 @@ var gulp = require('gulp');
 var help = require('gulp-task-listing');
 var cp = require('child_process');
 var sync = require('browser-sync');
-
-
+var repos = {
+  staging: {
+    url: 'git@github.com:tamouse/practical-php-primer-course.git',
+    branch: 'gh-pages'
+  }
+};
+var buildDir = '_site';
 gulp.task('help', help);
 gulp.task('default', help);
 gulp.task('build', function () {
-    //return cp.spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'});
-    return cp.execSync('bundle exec jekyll build', {stdio: 'inherit'})
+  //return cp.spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'});
+  return cp.execSync('bundle exec jekyll build', {stdio: 'inherit'})
 });
+gulp.task('build:stage', function () {
+  //return cp.spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'});
+  return cp.execSync('bundle exec jekyll build --config=_config.yml,_staging.yml', {stdio: 'inherit'})
+});
+gulp.task('push:stage', function () {
+  // return cp.execSync('' );
+});
+
 gulp.task('serve', ['build'], function(){
-    "use strict";
     sync.init({
         server: './_site'
     });
@@ -31,4 +43,7 @@ gulp.task('serve', ['build'], function(){
        '_config.yml'
    ],
    ['build'])
+});
+gulp.task('stage', ['build:stage', 'push:stage'], function () {
+  console.log('pushed to staging')
 });
